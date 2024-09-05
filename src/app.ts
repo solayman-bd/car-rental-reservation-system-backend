a1a67ff672ca61fd774338e338ca1b3313a153c6
solayman-bd <mdsolaymannuhin@gmail.com>
2024-09-06 00:40:47 +0600

    cors related issue solved

--- src/app.ts
+++ src/app.ts
@@ -10,11 +10,17 @@ const app: Application = express();
 // Middleware for parsing JSON and cookies
 app.use(express.json({ limit: '50mb' }));
 app.use(cookieParser());
-const corsOptions = {
-  origin: 'http://localhost:5173/',
-  credentials: true,
-};
-app.use(cors(corsOptions));
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      callback(null, true); // Accepts requests from all origins
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true, // Allows credentials (cookies, authorization headers)
+    allowedHeaders: ['Content-Type', 'Authorization'], // Customize headers as needed
+  }),
+);
 
 // Root testing route
 app.get('/', (req, res) => {
